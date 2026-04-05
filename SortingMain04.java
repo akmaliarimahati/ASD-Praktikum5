3a5653906d1a714f7cb394be3961c1b090027665
akmaliarimahati <akmaliarimahati09@gmail.com>
2026-04-05 09:03:10 +0700

    percobaan selection sort

--- SortingMain04.java
+++ SortingMain04.java
@@ -1,12 +1,21 @@
 public class SortingMain04 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+
         Sorting04 dataurut1 = new Sorting04(a, a.length);
+        Sorting04 dataurut2 = new Sorting04(a, a.length);
 
         System.out.println("Data Awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data Sudah Diurutkan Dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        System.out.println("\nData Awal 2");
+        dataurut2.tampil();
+        dataurut2.bubbleSort();
+        System.out.println("Data Sudah Diurutkan Dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
