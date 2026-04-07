319684722e12b1ab3bd381aa82d96bf5560b79c9
akmaliarimahati <akmaliarimahati09@gmail.com>
2026-04-07 07:21:16 +0700

    percobaan praktikum ke 2

---
+++ Mahasiswa04.java
@@ -0,0 +1,26 @@
+public class Mahasiswa04 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    //Konstruktor default
+    Mahasiswa04() {
+
+    }
+
+    //Konsruktor parameter
+    Mahasiswa04(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
---
+++ MahasiswaBerprestasi04.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi04 {
+    Mahasiswa04[] listMhs = new Mahasiswa04[5];
+    int idx;
+
+    void tambah(Mahasiswa04 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data Sudah Penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa04 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-----------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - 1; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa04 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
